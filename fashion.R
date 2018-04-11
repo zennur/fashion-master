c3fcad3ecc78d5c53c42d4eb9b4038efa70aec04
zennur <zehranurro@gmail.com>
2018-04-11 14:28:09 +0900

    Update fashion.R

--- fashion.R
+++ fashion.R
@@ -1,10 +1,3 @@
-# Load the MNIST digit recognition dataset into R
-# http://yann.lecun.com/exdb/mnist/
-# assume you have all 4 files and gunzip'd them
-# creates train$n, train$x, train$y  and test$n, test$x, test$y
-# e.g. train$x is a 60000 x 784 matrix, each row is one digit (28x28)
-# call:  show_digit(train$x[5,])   to see a digit.
-# brendan o'connor - gist.github.com/39760 - anyall.org
 
 library(caret)
 library(png)
